cd1334f1e62d9ae539a5cc3d067dae213b810074
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-07-18 03:53:10 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt(){
 }
 
 function pressIt(){
-  $('input').on('keydown',function(){
+  $('input').on('keydown',function(key){
     if(key.which === 71){
       alert("You pressed down the g key");
     }
